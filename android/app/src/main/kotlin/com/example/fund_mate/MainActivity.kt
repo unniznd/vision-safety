60d3fd5a60ada028625ed72ac3bcd92b49e4272a
aanandunnis <aanandunnis@gmail.com>
2022-01-05 23:02:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fund_mate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fund_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FundMate());
+}
+
+class FundMate extends StatefulWidget {
+  @override
+  _FundMateState createState() => _FundMateState();
+}
+
+class _FundMateState extends State<FundMate> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
